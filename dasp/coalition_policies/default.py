e05771f7a9f3f04b64df75e9d6737f8e44e36bd5
Marco Ancona <marco.ancona@inf.ethz.ch>
2019-02-14 18:00:09 +0100

    clean up and refactor default policies

--- dasp/coalition_policies/default.py
+++ dasp/coalition_policies/default.py
@@ -2,8 +2,8 @@ from abc import ABC, abstractmethod
 import numpy as np
 
 
-def spaced_elements(array, numElems = 4):
-    return [x[len(x)//2] for x in np.array_split(np.array(array), numElems)]
+def spaced_elements(array, num_elems=4):
+    return [x[len(x)//2] for x in np.array_split(np.array(array), num_elems)]
 
 
 class AbstractPlayerIterator(ABC):
@@ -19,7 +19,6 @@ class AbstractPlayerIterator(ABC):
         self.i = 0
         self.kn = self.n_players
         self.ks = spaced_elements(range(self.n_players), self.kn)
-
 
     def set_n_steps(self, steps):
         self.kn = steps
@@ -75,23 +74,28 @@ class DefaultPlayerIterator(AbstractPlayerIterator):
         return mask.reshape(self.input_shape), mask.reshape(self.input_shape)
 
 
-
-
 class ImagePlayerIterator(AbstractPlayerIterator):
 
-    def __init__(self, input, random=False, merge_channels=False, coalition_patch_size=None):
-        self.merge_channels = merge_channels
-        self.coalition_patch_size = coalition_patch_size
+    def __init__(self, input, random=False, window_shape=(1, 1, 1)):
+        self.window_shape = window_shape
+        assert self.window_shape is not None, "window_shape cannot be None"
+        assert len(self.window_shape) == 3, "window_shape must contain 3 elements"
+        assert window_shape[-1] == input.shape[-1] or window_shape[-1] == 1, \
+            "last element of window_shape must be 1 or equal to the last dimension of the input"
+        assert self.window_shape[0] == 1 and self.window_shape[1] == 1, \
+            "Players can only be individual pixels at the moment"
         super(ImagePlayerIterator, self).__init__(input, random)
 
     def _input_shape_merged(self):
-        return self.input_shape if self.merge_channels is not True else self.input_shape[:-1] + (1,)
+        shape = list(self.input_shape)
+        if self.window_shape[-1] > 1:
+            shape[-1] = 1
+        return shape
 
     def _assert_input_compatibility(self, input):
         assert len(input.shape) == 4, 'ImagePlayerIterator requires an input with 4 dimensions'
 
     def _get_number_of_players_from_shape(self):
-        print (np.prod(self._input_shape_merged()))
         return np.prod(self._input_shape_merged())
 
     def _get_masks_for_index(self, i):
@@ -99,23 +103,24 @@ class ImagePlayerIterator(AbstractPlayerIterator):
         mask[self.permutation[i]] = 1
         mask = mask.reshape(self._input_shape_merged())
         mask_input = mask
-        if self.merge_channels:
+        if self.window_shape[-1] > 1:
             mask_input = np.repeat(mask, self.input_shape[-1], -1)
         return mask_input, mask
 
     def get_explanation_shape(self):
         return self._input_shape_merged()
 
-
-
-def main():
-    iter = ImagePlayerIterator(np.random.random((1, 2, 2, 3)), merge_channels=True, random=True)
-    print (iter.get_number_of_players())
-    print (iter.get_explanation_shape())
-    mask = next(iter)
-    print (mask.shape)
-    print (next(iter))
-    print (mask[0,0])
-
-if __name__ == "__main__":
-    main()
+#
+#
+# def main():
+#     iter = ImagePlayerIterator(np.random.random((1, 2, 2, 3)), window_shape=(1,1,1))
+#     print (iter.get_number_of_players())
+#     print (iter.get_explanation_shape())
+#     mask, mask_out = next(iter)
+#     print (mask.shape)
+#     print (mask_out.shape)
+#     print (next(iter))
+#     print (mask[0,0])
+#
+# if __name__ == "__main__":
+#     main()
