Fix wrong computation on mean and variance in Dense input. Closes #1

dasp/dasp.py
@@ -23,7 +23,7 @@ class DASP():
         if self.dasp_model is not None:
             self.dasp_model.summary()
 
-    def run(self, x, steps=None):
+    def run(self, x, steps=None, batch_size=32):
         player_generator = self.player_generator
         if player_generator is None:
             player_generator = DefaultPlayerIterator(x)
@@ -41,9 +41,10 @@ class DASP():
         tile_mask = [len(ks)*x.shape[0]] + (len(x.shape) - 1) * [1]
 
         for i, (mask, mask_output) in enumerate(player_generator):
-            # Workaround: Keras does not seem to support scalar inputs
+            # Workaround: as Keras requires the first dimension of the inputs to be the same,
+            # we tile and repeat the input, mask and ks vector to have them aligned.
             inputs = [np.tile(x, tile_input), np.tile(mask, tile_mask), np.repeat(ks, x.shape[0])]
-            y1, y2 = self.dasp_model.predict(inputs)
+            y1, y2 = self.dasp_model.predict(inputs, batch_size=batch_size)
             y1 = y1.reshape(len(ks), x.shape[0], -1, 2)
             y2 = y2.reshape(len(ks), x.shape[0], -1, 2)
             y = np.mean(y2[..., 0] - y1[..., 0], 0)

dasp/layers/dense.py
@@ -33,14 +33,14 @@ class ProbDenseInput(Dense):
         mu = dot_i / dot_mask
         v = dot_v / dot_mask - mu ** 2
         # Compensate for number of players in current coalition
-        mu1 = mu * k[0,0]
+        mu1 = mu * k
         # Compute mean of the distribution that also includes player i (acting as bias to expectation)
         mu2 = mu1 + (dot - dot_i)
         # Compensate for number or players in the coalition
-        v1 = v * k[0,0] * (1.0 - (k[0,0]-1) / (dot_mask - 1))
+        v1 = v * k * (1.0 - (k-1) / (dot_mask - 1))
         # Set something different than 0 if necessary
         v1 = tf.maximum(0.00001, v1)
-        # Since player i is only a bias, at this point the variance of the distribution than
+        # Since player i is only a bias, at this point the variance of the distribution that
         # includes it is the same
         v2 = tf.identity(v1)
 